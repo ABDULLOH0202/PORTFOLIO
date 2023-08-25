de323cab2ced56a2783757c6b0be639435d55e54
abdulloh0202 <abdulloh0202@gmail.com>
2023-08-25 09:45:46 -0700

    Abdulloh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
-        <h1>Abdulloh</h1>
+        <h1></h1>
       </div>
 
       {/* menu */}
